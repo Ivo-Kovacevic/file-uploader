eb7de237b8fbb493b12060eaee236a0e81288ac8
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-09-26 20:17:13 +0200

    add fade in and fade out animations for tech stack

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,14 +1,11 @@
 const lenis = new Lenis();
-
 lenis.on("scroll", (e) => {
     console.log(e);
 });
-
 function raf(time) {
     lenis.raf(time);
     requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
 
 gsap.registerPlugin(ScrollTrigger);
@@ -20,8 +17,8 @@ function heroAnimation(target) {
                 trigger: target,
                 start: "top 20%",
                 end: "bottom top",
-                scrub: true,
-                markers: true,
+                scrub: 2,
+                markers: false,
             },
         })
         .to(target, {
@@ -33,20 +30,25 @@ function heroAnimation(target) {
 heroAnimation(".hero");
 heroAnimation(".arrow");
 
-function techStackAnimation(target) {
+function fadeTechStack(target, direction = "in") {
     return gsap
         .timeline({
             scrollTrigger: {
                 trigger: target,
-                start: "top 20%",
-                end: "bottom top",
-                scrub: true,
-                markers: true,
+                start: direction === "in" ? "top bottom" : "top 10%",
+                end: direction === "in" ? "bottom 90%" : "bottom top",
+                scrub: 2,
+                markers: false,
             },
         })
-        .to(target, {
-            x: 400,
-            opacity: 0,
-            duration: 1,
-        });
+        .fromTo(
+            target,
+            { x: direction === "in" ? -500 : 0, opacity: direction === "in" ? 0 : 1 },
+            { x: direction === "in" ? 0 : 500, opacity: direction === "in" ? 1 : 0, duration: 1 }
+        );
 }
+
+[".node-express", ".passport", ".postgre", ".prisma", ".tailwind", ".gsap"].forEach((target) => {
+    fadeTechStack(target, "in");
+    fadeTechStack(target, "out");
+});
